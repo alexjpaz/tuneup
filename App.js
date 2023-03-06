03ef95a552e1623d1db71557e4fa1c0e4304a02f
Alexander Paz <alexjpaz@gmail.com>
2023-03-06 14:58:29 -0500

    Adding react native paper

--- App.js
+++ App.js
@@ -1,44 +1,24 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-import { Audio } from 'expo-av';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import ScalePlayer from './ScalePlayer';
 
 export default function App() {
-
-  const [sound, setSound] = React.useState();
-
-  async function playSound() {
-    console.log('Loading Sound');
-    const { sound } = await Audio.Sound.createAsync( require('./assets/foo.mp3')
-    );
-    setSound(sound); 
-
-    console.log('Playing Sound');
-    await sound.playAsync();
-  }
-
-  React.useEffect(() => {
-    return sound
-      ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync();
-        }
-      : undefined;
-  }, [sound]);
-
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Play Sound" onPress={playSound} />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <ScalePlayer />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffa',
+    backgroundColor: '#242c40',
     alignItems: 'center',
     justifyContent: 'center',
   },
