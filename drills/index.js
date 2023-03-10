635bc020d5b59ce5f782f7ca7404ca77eb6fda2d
Alexander Paz <alexjpaz@gmail.com>
2023-03-10 11:32:10 -0500

    testing midi viewport

--- drills/index.js
+++ drills/index.js
@@ -13,11 +13,11 @@ const ranges = {
 };
 
 track.addEvent([
-  new MidiWriter.NoteEvent({pitch: Scale.get("C1 major").notes, duration: '1', velocity: '100'}),
-  new MidiWriter.NoteEvent({pitch: Scale.get("C2 major").notes, duration: '1', velocity: '100'}),
-  new MidiWriter.NoteEvent({pitch: Scale.get("C3 major").notes, duration: '1', velocity: '100'}),
-  new MidiWriter.NoteEvent({pitch: Scale.get("C4 major").notes, duration: '1', velocity: '100'}),
-  new MidiWriter.NoteEvent({pitch: Scale.get("C5 major").notes, duration: '1', velocity: '100'}),
+  new MidiWriter.NoteEvent({pitch: Scale.get("C1 major").notes, duration: '4', velocity: '100'}),
+  new MidiWriter.NoteEvent({pitch: Scale.get("C2 major").notes, duration: '4', velocity: '100'}),
+  new MidiWriter.NoteEvent({pitch: Scale.get("C3 major").notes, duration: '4', velocity: '100'}),
+  new MidiWriter.NoteEvent({pitch: Scale.get("C4 major").notes, duration: '4', velocity: '100'}),
+  new MidiWriter.NoteEvent({pitch: Scale.get("C5 major").notes, duration: '4', velocity: '100'}),
 ], function(event, index) {
   return {sequential: true};
 });
