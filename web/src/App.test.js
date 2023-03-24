f3e57ab46a2f3a4dd5bf2e2910d6a84fc5f454c2
Alexander Paz <alexjpaz@gmail.com>
2023-03-24 08:28:15 -0400

    Adding some app tests

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Daily Guided Practice/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("renders basic components of the app", () => {
+  
+  test('Daily Guided Practice', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Daily Guided Practice/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('Drill Playlist', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Drill Playlist/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('Midi Player', () => {
+    render(<App />);
+    const linkElement = screen.getByTitle(/midi-player/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+})
+
+
