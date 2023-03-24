70cf3aeddd65ebe92a7362fa14784470718553b3
Alexander Paz <alexjpaz@gmail.com>
2023-03-24 10:57:40 -0400

    refactoring components

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,12 @@
 
 import React from 'react';
 import './App.css';
-import DrillList from './DrillList';
-import MidiPlayer from './MidiPlayer';
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { indigo, pink } from '@mui/material/colors';
 
-import { Box, IconButton, Paper } from '@mui/material';
-import { Container } from '@mui/system';
-import { Menu as MenuIcon } from '@mui/icons-material';
+import MainScreen from './MainScreen';
 
 const darkTheme = createTheme({
   palette: {
@@ -25,68 +21,13 @@ const darkTheme = createTheme({
 
 function App() {
 
-  const [state, setState] = React.useState();
-
-  const onSelected = (action) => {
-    setState(action.drill.data);
-
-  };
-
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline>
-        <Box sx={{position: "fixed", left: "1rem", top: "1rem"}} data-testid="main-menu">
-          <IconButton>
-            <MenuIcon />
-          </IconButton>
-        </Box>
-        <Container maxWidth="sm" style={styles.container} data-testid="main-container">
-          <Paper sx={{flex: 4, overflow: "hidden", display: "flex"}} elevation={2}>
-            <MidiPlayer src={state} />
-          </Paper>
-          <Paper sx={{flex:  5, overflow: "auto"}} elevation={12}>
-            <DrillList onSelected={onSelected} />
-          </Paper>
-        </Container>
+        <MainScreen />
       </CssBaseline>
     </ThemeProvider >
   );
-}
-
-
-/*
-<Paper style={styles.player}>
-              <MidiPlayer src={state} />
-            </Paper>
-            <Paper style={styles.list}>
-            </Paper>
-*/
-
-const styles = {
-  container: {
-     height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    padding: 0,
-    
-  },
-  flex: {
-    display: "flex",
-
-  },
-  flexChild: {
-    flex: 1,
-  },
-
-  player: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-  },
-  list: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    overflow: 'auto'
-  }
 }
 
 export default App;
