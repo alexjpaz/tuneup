b7c8a6c5a2a97e6e26ec7d4b68ba332ec6e25dcb
Alexander Paz <alexjpaz@gmail.com>
2023-03-28 14:42:04 -0400

    Adding more ranges and fixing scales

--- drills/src/common/ranges.js
+++ drills/src/common/ranges.js
@@ -2,10 +2,18 @@ const { Interval, Note, Scale } = require("tonal");
 
 
 const ranges = {
+  bass: {
+    start: "E2",
+    end: "E4",
+  },
   baritone: {
     start: "G2",
     end: "G4",
-  }
+  },
+  baritone: {
+    start: "C3",
+    end: "C5",
+  },
 };
 
 module.exports = ranges;
